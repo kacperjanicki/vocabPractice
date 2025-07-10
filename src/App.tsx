1532e28557b5bc6d2beaf772c3b6905a11f0ffe2
kacperjanicki <kacperekjanicki9@gmail.com>
2025-07-10 11:43:24 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  console.log("test")
+
+  return (
+    <>
+      <h1> Hello react</h1>
+    </>
+  )
+}
+
+export default App
